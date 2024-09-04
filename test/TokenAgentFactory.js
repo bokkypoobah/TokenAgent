8dc01e14f066671754318c6a02cbb7c006adfb75
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-09-04 11:31:38 +1000

    wip

--- test/TokenAgentFactory.js
+++ test/TokenAgentFactory.js
@@ -16,38 +16,6 @@ const FILLORKILL = 1;
 
 describe("TokenAgentFactory", function () {
 
-  async function deployContracts() {
-    const accounts = await ethers.getSigners();
-    console.log("        * accounts: " + JSON.stringify(accounts.slice(0, 3).map(e => e.address)));
-    const WETH9 = await ethers.getContractFactory("WETH9");
-    const weth9 = await WETH9.deploy();
-    const ERC20Token = await ethers.getContractFactory("ERC20Token");
-    const erc20Token = await ERC20Token.deploy();
-    const ERC721Token = await ethers.getContractFactory("ERC721Token");
-    const erc721Token = await ERC721Token.deploy();
-    const ERC1155Token = await ethers.getContractFactory("ERC1155Token");
-    const erc1155Token = await ERC1155Token.deploy("https://blah.blah/blah/");
-    console.log("        * weth: " + weth9.target);
-    console.log("        * 20: " + erc20Token.target + ", 721: "+ erc721Token.target + ", 1155: "+ erc1155Token.target);
-    const TokenAgentFactory = await ethers.getContractFactory("TokenAgentFactory");
-    const tokenAgentFactory = await TokenAgentFactory.deploy(weth9);
-
-    const amountWeth = ethers.parseUnits("100", 18);
-    await accounts[0].sendTransaction({ to: weth9.target, value: amountWeth });
-    await accounts[1].sendTransaction({ to: weth9.target, value: amountWeth });
-    await accounts[2].sendTransaction({ to: weth9.target, value: amountWeth });
-    await accounts[3].sendTransaction({ to: weth9.target, value: amountWeth });
-    // const weth0Tx = await data.user0Signer.sendTransaction({ to: data.weth.address, value: amountWeth });
-
-    const amountERC20 = ethers.parseUnits("1000", 18);
-    await erc20Token.transfer(accounts[1], amountERC20);
-    await erc20Token.transfer(accounts[2], amountERC20);
-    await erc20Token.transfer(accounts[3], amountERC20);
-    await erc20Token.transfer(ADDRESS0, ethers.parseUnits("996000", 18));
-
-    return { tokenAgentFactory, weth9, erc20Token, erc721Token, erc1155Token, accounts };
-  }
-
   function padLeft(s, n) {
     var o = s.toString();
     while (o.length < n) {
@@ -55,21 +23,20 @@ describe("TokenAgentFactory", function () {
     }
     return o;
   }
-  // function padRight(s, n) {
-  //   var o = s;
-  //   while (o.length < n) {
-  //     o = o + " ";
-  //   }
-  //   return o;
-  // }
-
+  function padRight(s, n) {
+    var o = s;
+    while (o.length < n) {
+      o = o + " ";
+    }
+    return o;
+  }
   async function printState(data) {
     console.log();
     console.log("          # Account                               ETH                     WETH                   ERC-20");
     console.log("          - ---------------- ------------------------ ------------------------ ------------------------");
     for (let i = 0; i < 4; i++) {
       const balance = await ethers.provider.getBalance(data.accounts[i].address);
-      const wethBalance = await data.weth9.balanceOf(data.accounts[i].address);
+      const wethBalance = await data.weth.balanceOf(data.accounts[i].address);
       const erc20Balance = await data.erc20Token.balanceOf(data.accounts[i].address);
       console.log("          " + i + " " + data.accounts[i].address.substring(0, 16) + " " +
         padLeft(ethers.formatEther(balance), 24) + " " +
@@ -80,97 +47,107 @@ describe("TokenAgentFactory", function () {
     console.log();
   }
 
+  async function deployContracts() {
+    const accounts = await ethers.getSigners();
+    console.log("        * accounts: " + JSON.stringify(accounts.slice(0, 3).map(e => e.address)));
+    const WETH9 = await ethers.getContractFactory("WETH9");
+    const weth = await WETH9.deploy();
+    const ERC20Token = await ethers.getContractFactory("ERC20Token");
+    const erc20Token = await ERC20Token.deploy();
+    const ERC721Token = await ethers.getContractFactory("ERC721Token");
+    const erc721Token = await ERC721Token.deploy();
+    const ERC1155Token = await ethers.getContractFactory("ERC1155Token");
+    const erc1155Token = await ERC1155Token.deploy("https://blah.blah/blah/");
+    console.log("        * weth: " + weth.target);
+    console.log("        * 20: " + erc20Token.target + ", 721: "+ erc721Token.target + ", 1155: "+ erc1155Token.target);
+    const TokenAgentFactory = await ethers.getContractFactory("TokenAgentFactory");
+    const tokenAgentFactory = await TokenAgentFactory.deploy(weth);
+
+    const TokenAgent = await ethers.getContractFactory("TokenAgent");
+    const tokenAgents = [];
+    for (let i = 0; i < 4; i++) {
+      await tokenAgentFactory.connect(accounts[i]).newTokenAgent();
+      const tokenAgentAddress = await tokenAgentFactory.tokenAgentsByOwners(accounts[i].address, 0);
+      const tokenAgent = TokenAgent.attach(tokenAgentAddress);
+      tokenAgents.push(tokenAgent);
+    }
+
+    const amountWeth = ethers.parseUnits("100", 18);
+    for (let i = 0; i < 4; i++) {
+      await accounts[i].sendTransaction({ to: weth.target, value: amountWeth });
+    }
+
+    const amountERC20 = ethers.parseUnits("1000", 18);
+    for (let i = 1; i < 4; i++) {
+      await erc20Token.transfer(accounts[i], amountERC20);
+    }
+    await erc20Token.transfer(ADDRESS0, ethers.parseUnits("996000", 18));
+
+    const approveAmount = ethers.parseUnits("12.345", 18);
+    for (let i = 1; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        await weth.connect(accounts[i]).approve(tokenAgents[j], approveAmount);
+        await erc20Token.connect(accounts[i]).approve(tokenAgents[j], approveAmount);
+      }
+    }
+
+    const now = parseInt(new Date().getTime()/1000);
+    const expiry60s = now + 60 * 1000;
+
+    return { tokenAgentFactory, tokenAgents, weth, erc20Token, erc721Token, erc1155Token, accounts, now, expiry60s };
+  }
+
   describe("Deploy TokenAgentFactory And TokenAgent", function () {
 
-    it("Basic deployment", async function () {
-      const { tokenAgentFactory, accounts } = await loadFixture(deployContracts);
-      const tokenAgentTemplate = await tokenAgentFactory.tokenAgentTemplate();
-      await expect(tokenAgentFactory.newTokenAgent())
-        .to.emit(tokenAgentFactory, "NewTokenAgent")
-        .withArgs(anyValue, accounts[0].address, 0, anyValue);
-      const tokenAgentAddress = await tokenAgentFactory.tokenAgents(0);
-      const tokenAgentByOwner = await tokenAgentFactory.tokenAgentsByOwners(accounts[0].address, 0);
-      expect(await tokenAgentFactory.tokenAgentsByOwners(accounts[0].address, 0)).to.equal(tokenAgentAddress);
-      const TokenAgent = await ethers.getContractFactory("TokenAgent");
-      const tokenAgent = TokenAgent.attach(tokenAgentAddress);
-      const tokenAgentOwner = await tokenAgent.owner();
-    });
-
     it("Test TokenAgent ownership", async function () {
-      const { tokenAgentFactory, weth9, accounts } = await loadFixture(deployContracts);
-      await expect(tokenAgentFactory.newTokenAgent())
-        .to.emit(tokenAgentFactory, "NewTokenAgent")
-        .withArgs(anyValue, accounts[0].address, 0, anyValue);
-      const tokenAgentAddress = await tokenAgentFactory.tokenAgents(0);
+      const d = await loadFixture(deployContracts);
+      await expect(d.tokenAgentFactory.newTokenAgent())
+        .to.emit(d.tokenAgentFactory, "NewTokenAgent")
+        .withArgs(anyValue, d.accounts[0].address, 1, anyValue);
+      const tokenAgentAddress = await d.tokenAgentFactory.tokenAgents(4);
       const TokenAgent = await ethers.getContractFactory("TokenAgent");
       const tokenAgent = TokenAgent.attach(tokenAgentAddress);
-      await expect(tokenAgent.connect(accounts[1]).init(weth9, accounts[1]))
+      await expect(tokenAgent.connect(d.accounts[1]).init(d.weth, d.accounts[1]))
         .to.be.revertedWithCustomError(tokenAgent, "AlreadyInitialised");
-      const tokenAgentOwner = await tokenAgent.owner();
-      await expect(tokenAgent.connect(accounts[1]).transferOwnership(accounts[0]))
+      expect(await tokenAgent.owner()).to.equal(d.accounts[0].address);
+      await expect(tokenAgent.connect(d.accounts[1]).transferOwnership(d.accounts[0]))
         .to.be.revertedWithCustomError(tokenAgent, "NotOwner");
-      await tokenAgent.connect(accounts[0]).transferOwnership(accounts[1]);
-      await expect(tokenAgent.connect(accounts[2]).acceptOwnership())
+      await tokenAgent.connect(d.accounts[0]).transferOwnership(d.accounts[1]);
+      await expect(tokenAgent.connect(d.accounts[2]).acceptOwnership())
         .to.be.revertedWithCustomError(tokenAgent, "NotNewOwner");
-      await expect(tokenAgent.connect(accounts[1]).acceptOwnership())
+      await expect(tokenAgent.connect(d.accounts[1]).acceptOwnership())
         .to.emit(tokenAgent, "OwnershipTransferred")
-        .withArgs(accounts[0].address, accounts[1].address, anyValue);
+        .withArgs(d.accounts[0].address, d.accounts[1].address, anyValue);
+      // await printState(d);
     });
 
     it("Test TokenAgent invalid offers", async function () {
-      const { tokenAgentFactory, weth9, erc20Token, erc721Token, erc1155Token, accounts } = await loadFixture(deployContracts);
-      await expect(tokenAgentFactory.newTokenAgent())
-        .to.emit(tokenAgentFactory, "NewTokenAgent")
-        .withArgs(anyValue, accounts[0].address, 0, anyValue);
-      const tokenAgentAddress = await tokenAgentFactory.tokenAgents(0);
+      const d = await loadFixture(deployContracts);
+      await expect(d.tokenAgentFactory.newTokenAgent())
+        .to.emit(d.tokenAgentFactory, "NewTokenAgent")
+        .withArgs(anyValue, d.accounts[0].address, 1, anyValue);
+      const tokenAgentAddress = await d.tokenAgentFactory.tokenAgents(4);
       const TokenAgent = await ethers.getContractFactory("TokenAgent");
       const tokenAgent = TokenAgent.attach(tokenAgentAddress);
-
-      const now = parseInt(new Date().getTime()/1000);
-      const expiry = now + 60 * 1000;
-
-      const invalidOffer1 = [
-        [accounts[0].address, SELL, expiry, 888, "999999999999999999999999999999999997"],
-      ];
+      const invalidOffer1 = [[d.accounts[0].address, SELL, d.expiry60s, [[888, "999999999999999999999999999999999997"]]]];
       await expect(tokenAgent.addOffers(invalidOffer1))
         .to.be.revertedWithCustomError(tokenAgent, "InvalidToken")
-        .withArgs(accounts[0].address);
+        .withArgs(d.accounts[0].address);
       const invalidOffer2 = [
-        [weth9.target, SELL, expiry, 888, "999999999999999999999999999999999997"],
+        [d.weth.target, SELL, d.expiry60s, [[888, "999999999999999999999999999999999997"]]],
       ];
       await expect(tokenAgent.addOffers(invalidOffer2))
         .to.be.revertedWithCustomError(tokenAgent, "CannotOfferWETH");
     });
 
-    it.only("Test TokenAgent offers", async function () {
-      const data = await loadFixture(deployContracts);
-      const { tokenAgentFactory, weth9, erc20Token, erc721Token, erc1155Token, accounts } = data;
-      await expect(tokenAgentFactory.connect(accounts[1]).newTokenAgent())
-        .to.emit(tokenAgentFactory, "NewTokenAgent")
-        .withArgs(anyValue, accounts[1].address, 0, anyValue);
-      const tokenAgentAddress = await tokenAgentFactory.tokenAgents(0);
-      const TokenAgent = await ethers.getContractFactory("TokenAgent");
-      const tokenAgent = TokenAgent.attach(tokenAgentAddress);
-
-      const approveAmount = ethers.parseUnits("10000.00001", 18);
-      await weth9.connect(accounts[0]).approve(tokenAgentAddress, approveAmount);
-      await weth9.connect(accounts[1]).approve(tokenAgentAddress, approveAmount);
-      await weth9.connect(accounts[2]).approve(tokenAgentAddress, approveAmount);
-      await weth9.connect(accounts[3]).approve(tokenAgentAddress, approveAmount);
-      await erc20Token.connect(accounts[0]).approve(tokenAgentAddress, approveAmount);
-      await erc20Token.connect(accounts[1]).approve(tokenAgentAddress, approveAmount);
-      await erc20Token.connect(accounts[2]).approve(tokenAgentAddress, approveAmount);
-      await erc20Token.connect(accounts[3]).approve(tokenAgentAddress, approveAmount);
-
-      await printState(data);
-
-      const now = parseInt(new Date().getTime()/1000);
-      const expiry = now + 60 * 1000; // 0 ok, 1 fail, <now fail
+    it("Test TokenAgent offers", async function () {
+      const d = await loadFixture(deployContracts);
+      await printState(d);
 
       const offers1 = [
-        [ erc20Token.target,
+        [ d.erc20Token.target,
           SELL,
-          expiry,
+          d.expiry60s,
           [
             [ethers.parseUnits("0.1", 18), ethers.parseUnits("1", 18)],
             [ethers.parseUnits("0.2", 18), ethers.parseUnits("1", 18)],
@@ -182,13 +159,12 @@ describe("TokenAgentFactory", function () {
         // [erc721Token.target, BUY, expiry, ethers.parseUnits("0.123456", 18), ethers.parseUnits("1000.2", 18)],
         // [erc1155Token.target, SELL, expiry, ethers.parseUnits("0.1234567", 18), ethers.parseUnits("1000.3", 18)],
       ];
-      const addOffers1Tx = await tokenAgent.connect(accounts[1]).addOffers(offers1);
+      const addOffers1Tx = await d.tokenAgents[1].connect(d.accounts[1]).addOffers(offers1);
       const addOffers1TxReceipt = await addOffers1Tx.wait();
       console.log("        * addOffers1TxReceipt.gasUsed: " + addOffers1TxReceipt.gasUsed);
-      // console.log("        * addOffers1TxReceipt: " + JSON.stringify(addOffers1TxReceipt, null, 2));
       const offerKeys = [];
       addOffers1TxReceipt.logs.forEach((event) => {
-        const log = tokenAgent.interface.parseLog(event);
+        const log = d.tokenAgents[1].interface.parseLog(event);
         offerKeys.push(log.args[0]);
         // console.log("        + " + log.name + JSON.stringify(log.args.map(e => e.toString())));
         console.log("        + " + log.name + '(key:' + log.args[0].substring(0, 20) + ', token: ' + log.args[1].substring(0, 12) + ', nonce: ' + log.args[2] + ', info: ' + JSON.stringify(log.args[3].map(e => e.toString())) + ') @ ' + new Date(parseInt(log.args[4]) * 1000).toLocaleString());
@@ -201,36 +177,23 @@ describe("TokenAgentFactory", function () {
         // [offerKeys[2], ethers.parseUnits("30", 18).toString()]
       ];
       console.log("        * trades1: " + JSON.stringify(trades1));
-      const trades1Tx = await tokenAgent.connect(accounts[2]).trade(trades1);
+      const trades1Tx = await d.tokenAgents[1].connect(d.accounts[2]).trade(trades1);
       const trades1TxReceipt = await trades1Tx.wait();
       console.log("        * trades1TxReceipt.gasUsed: " + trades1TxReceipt.gasUsed);
       trades1TxReceipt.logs.forEach((event) => {
-        if (event.address == weth9.target) {
-          const log = weth9.interface.parseLog(event);
-          console.log("        + weth9." + log.name + '(' + log.args.join(',') + ')');
-        } else if (event.address == erc20Token.target) {
-          const log = erc20Token.interface.parseLog(event);
+        if (event.address == d.weth.target) {
+          const log = d.weth.interface.parseLog(event);
+          console.log("        + weth." + log.name + '(' + log.args.join(',') + ')');
+        } else if (event.address == d.erc20Token.target) {
+          const log = d.erc20Token.interface.parseLog(event);
           console.log("        + erc20Token." + log.name + '(' + log.args.join(',') + ')');
-        } else if (event.address == tokenAgent.target) {
-          const log = tokenAgent.interface.parseLog(event);
+        } else if (event.address == d.tokenAgents[1].target) {
+          const log = d.tokenAgents[1].interface.parseLog(event);
           console.log("        + " + log.name + '(' + log.args.join(',') + ')');
         }
       });
 
-      await printState(data);
-
-      // const trades2 = [
-      //   // [offerKeys[0], ethers.parseUnits("10", 18).toString()],
-      //   [offerKeys[1], ethers.parseUnits("1", 18).toString()],
-      //   // [offerKeys[2], ethers.parseUnits("30", 18).toString()]
-      // ];
-      // console.log("        * trades2: " + JSON.stringify(trades2));
-      // const trades2Tx = await tokenAgent.connect(accounts[2]).trade(trades2);
-      // const trades2TxReceipt = await trades2Tx.wait();
-      // console.log("        * trades2TxReceipt.gasUsed: " + trades2TxReceipt.gasUsed);
-      //
-      // await printState(data);
-
+      await printState(d);
     });
 
   });
